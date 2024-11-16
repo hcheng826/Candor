7920417debbb07d623cd51d99dd38eafd3ea34a5
andreasss55 <andreas@breeze.cash>
2024-11-16 17:01:33 +0700

    update login

--- frontend/src/components/LoginWallet/UserWallet.tsx
+++ frontend/src/components/LoginWallet/UserWallet.tsx
@@ -1,20 +1,102 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   DynamicUserProfile,
   useDynamicContext,
 } from "@dynamic-labs/sdk-react-core";
 import { CircleUserIcon } from "lucide-react";
+import { useWallet } from "@/hooks/useWallet";
+import Image from "next/image";
+import {
+  MenuContent,
+  MenuItem,
+  MenuRoot,
+  MenuTrigger,
+} from "@/components/ui/menu";
+import { Button } from "../ui/button";
+import { BLOCKCHAIN_CONFIGS } from "@/config";
+import { useMediaQuery } from "@chakra-ui/react";
+import { useIsMobile } from "@/hooks/isMobile";
+import { BLOCKCHAIN_NETWORK } from "@/types";
 
 export const UserWallet = () => {
   const { setShowDynamicUserProfile } = useDynamicContext();
+
   return (
-    <div>
-      <button onClick={() => setShowDynamicUserProfile(true)}>
-        <CircleUserIcon width={30} height={30} style={{
-            opacity: '0.5'
-        }} />
+    <div className="flex items-center">
+      <ChainDropdown />
+      <button onClick={() => setShowDynamicUserProfile(true)} className="ml-2">
+        <CircleUserIcon
+          width={30}
+          height={30}
+          style={{
+            opacity: "0.5",
+          }}
+        />
       </button>
       <DynamicUserProfile />
     </div>
+  );
+};
+
+const ChainDropdown = () => {
+  const { connectedChain, setChain, isMiniApp } = useWallet();
+  const isMobile = useIsMobile();
+
+  //fallback to world main if mini app
+  let finalChain = connectedChain;
+  if (isMiniApp) {
+    finalChain = BLOCKCHAIN_CONFIGS.find(
+      (c) => c.id === BLOCKCHAIN_NETWORK.WORLD_MAIN
+    );
+  }
+
+  useEffect(() => {
+    if (isMiniApp && finalChain) {
+      setChain(finalChain.chainIdNum);
+    }
+  }, [isMiniApp]);
+
+  return (
+    <MenuRoot>
+      <MenuTrigger asChild>
+        <Button
+          variant="ghost"
+          size="sm"
+          background={"transparent"}
+          className="text-sm"
+        >
+          {
+            <Image
+              src={finalChain?.logoUrl || ""}
+              alt=""
+              width={30}
+              height={30}
+              className="mr-1"
+            />
+          }
+
+          {!isMobile && finalChain?.name}
+        </Button>
+      </MenuTrigger>
+      <MenuContent className="bg-white text-black">
+        {BLOCKCHAIN_CONFIGS.map((item) => (
+          <MenuItem
+            value={item.id}
+            className="text-gray-600 hover:bg-gray-100  cursor-pointer text-sm"
+            onClick={() => setChain(item.chainIdNum)}
+            key={item.name}
+          >
+            <Image
+              src={item?.logoUrl || ""}
+              alt=""
+              width={30}
+              height={30}
+              className="mr-1"
+            />
+            {item.name}
+          </MenuItem>
+        ))}
+      </MenuContent>
+    </MenuRoot>
   );
 };
--- frontend/src/components/LoginWallet/index.tsx
+++ frontend/src/components/LoginWallet/index.tsx
@@ -1,16 +1,25 @@
 import { DynamicEmbeddedWidget } from "@dynamic-labs/sdk-react-core";
 import { Container } from "@chakra-ui/react";
-import { useWallet } from "@/hooks/useWallet";
+import { useGlobalStore, useWallet } from "@/hooks/useWallet";
 import { MiniKit } from "@worldcoin/minikit-js";
 import { PrimaryButton } from "../Button/PrimaryButton";
 import { toaster, Toaster } from "../ui/toaster";
+import { useState } from "react";
+import { useShallow } from "zustand/react/shallow";
 
 export const LoginWallet = ({
   title = "Connect Your Wallet",
 }: {
   title?: string;
 }) => {
-  const { walletAddress, isMiniApp, localNonce } = useWallet();
+  const { refresh, setRefresh } = useGlobalStore(
+    useShallow((state) => ({
+      refresh: state.refresh,
+      setRefresh: state.setRefresh,
+    }))
+  );
+
+  const { walletAddress, isMiniApp, localNonce, miniAppAddress } = useWallet();
 
   const signInWithWorldIdWallet = async () => {
     if (!isMiniApp || !localNonce) {
@@ -36,6 +45,7 @@ export const LoginWallet = ({
         placement: "top-end",
         type: "error",
       });
+      setRefresh();
       return;
     } else {
       toaster.create({
@@ -44,6 +54,7 @@ export const LoginWallet = ({
         placement: "top-end",
         type: "success",
       });
+      setRefresh();
     }
   };
 
@@ -51,13 +62,14 @@ export const LoginWallet = ({
     <Container className="w-full max-w-[512px] bg-white">
       <div className="text-2xl font-bold text-center mb-6">{title}</div>
 
-      {/* {isMiniApp && (
-        <PrimaryButton onClick={signInWithWorldIdWallet}>
+      <div>Miniapp address: {miniAppAddress || "-"}</div>
+      {isMiniApp && !miniAppAddress && (
+        <PrimaryButton onClick={signInWithWorldIdWallet} className="w-full">
           Connect with WorldID
         </PrimaryButton>
-      )} */}
-      {/* {!isMiniApp && <DynamicEmbeddedWidget />} */}
-      <DynamicEmbeddedWidget />
+      )}
+      {!miniAppAddress && !walletAddress && <DynamicEmbeddedWidget />}
+      {/* <DynamicEmbeddedWidget /> */}
     </Container>
   );
 };
